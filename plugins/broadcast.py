53e5b469d53f25680b0abc40ebe493a3dc3d2fbd
Jins Mathew <jins.palakkattu@gmail.com>
2021-12-10 16:17:09 +0300

    Initial Commit

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -117,10 +117,10 @@ async def verupikkals(bot, message):
         #     elif sh == "Error":
         #         failed += 1
             done += 1
-            await asyncio.sleep(2)
-            if not done % 5:
-                await sts.edit_text(
-                    f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+            # await asyncio.sleep(2)
+            # if not done % 5:
+            #     await sts.edit_text(
+            #         f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
     time_taken = datetime.timedelta(seconds=int(time.time() - start_time))
     await sts.edit_text(
         f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
