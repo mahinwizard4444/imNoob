615b40c9907c952b91ff01cd28ed8fb92e22e178
Jins Mathew <jins.palakkattu@gmail.com>
2021-12-30 18:17:36 +0300

    Initial Commit

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂😍"""
     HELP_STRINGS = """
-Hey Dear <b>{}</b>! My name is <b>UFS #V3.0</b>>. I Am A Group Movie Bot, Here To Help You Get Around And Keep The Order In Your Groups!
+Hey Dear <b>{}</b>! My name is <b>UFS #V3.0</b>. I Am A Group Movie Bot, Here To Help You Get Around And Keep The Order In Your Groups!
 I Have Lots Of Handy Features, 
 
 <b>Helpful commands</b>:
